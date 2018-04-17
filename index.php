39682098c0f3c989503ebfe2abedc558b80ecce2
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-17 08:34:08 -0600

    built table, blockquote, and parahgraph w header

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Snap Challenge</title>
+    </head>
+    <body>
+    <h1>Hello everyone!</h1>
+    <p>This is our OH SNAP challenge today.</p>
+    <ul>
+        <li>Write some HTML</li>
+        <li>Map out the DOM</li>
+    </ul>
+    <blockquote>Mary had a little lamb, and her fleece was soft like cotton.</blockquote>
+    <table>
+        <tr>
+            <th>Lemon</th>
+            <th>Dragon's Egg</th>
+            <th>Mouse Melon</th>
+        </tr>
+        <tr>
+            <td>Round, yellow, mild in flavor</td>
+            <td>Smooth, white, medium, egg shaped</td>
+            <td>Tiny, little watermelons</td>
+        </tr>
+    </table>
+    </body>
+</html>
