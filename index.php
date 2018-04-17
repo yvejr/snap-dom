9c8f3a8204fa96121a20fa734b96d6c175a7ac9a
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-04-17 08:58:45 -0600

    added additional text and random tags

--- index.php
+++ index.php
@@ -11,18 +11,27 @@
         <li>Write some HTML</li>
         <li>Map out the DOM</li>
     </ul>
+    <p>Because I need twice as many block elements, Mozilla has said,
+        <q
+        cite="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/q">Read the documentation.</q>
+    </p>
+    <h2>A nice <big>non-rhyme</big>.</h2>
     <blockquote>Mary had a little lamb, and her fleece was soft like cotton.</blockquote>
     <table>
         <tr>
             <th>Lemon</th>
-            <th>Dragon's Egg</th>
+            <th><i>Dragon's</i> Egg</th>
             <th>Mouse Melon</th>
         </tr>
         <tr>
             <td>Round, yellow, mild in flavor</td>
             <td>Smooth, white, medium, egg shaped</td>
-            <td>Tiny, little watermelons</td>
+            <td>Tiny, <strike>sour</strike>, little watermelons</td>
         </tr>
     </table>
+    <p>We hope <strong>you</strong> have enjoyed our site!</p>
     </body>
+    <footer>
+        If you want to try <em>any</em> of these, visit me at <br><address>70000 Apple Avenue,<br> St. Paul MN, 00000</address>
+    </footer>
 </html>
